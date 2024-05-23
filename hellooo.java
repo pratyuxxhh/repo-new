8aca8181053c0cf1fc6cdc5481fa36876c1ac089
pratyuxxhh <ishu.kool2406@gmail.com>
2024-05-23 23:05:18 +0530

    first code

--- hellooo.java
+++ hellooo.java
@@ -1,6 +1,9 @@
 class hello {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String name = " divine ki maa" ;
+        System.out.println("dekh rha h n binod ,");
+        System.out.println(" kaise code parhne ki koshish ki ja rhi h");
+        System.out.println(name);
     }
  }
