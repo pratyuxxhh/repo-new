2062518b2ab46effe78020d46f2cd25c7fd556ef
pratyuxxhh <ishu.kool2406@gmail.com>
2024-05-23 23:27:52 +0530

    new changes made

--- hellooo.java
+++
@@ -1,9 +0,0 @@
-class hello {
-
-    public static void main(String[] args) {
-        String name = " divine ki maa" ;
-        System.out.println("dekh rha h n binod ,");
-        System.out.println(" kaise code parhne ki koshish ki ja rhi h");
-        System.out.println(name);
-    }
- }
--- new_number_c.java
+++ new_number_c.java
@@ -1,7 +1,10 @@
-public class new_number_c {
+class hello {
 
     public static void main(String[] args) {
-        int c = 81;
+        String name = " divine ki maa" ;
+        System.out.println("\n \ndekh rha h n binod ,");
+        System.out.println(" kaise code parhne ki koshish ki ja rhi h \n ");
+        System.out.print("name : ");
+        System.out.println(name);
     }
-
-}
+ }
