5feeb9f8ef133ade11eba9698ea08f297112cb6d
pratyuxxhh <ishu.kool2406@gmail.com>
2024-06-06 11:28:20 +0530

    comments added

--- TwoD_Array.java
+++ TwoD_Array.java
@@ -2,9 +2,14 @@ import java.util.*;
 public class TwoD_Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // enter size of row and column
         int row =sc.nextInt();   
         int col = sc.nextInt();
-        int [][] Array = new int[row][col];
+        int [][] Array = new int[row][col]; 
+
+
+        // to find a number from 2D matrix
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
